8de4762ae7d8b8399b5714b9c9034796253e3d54
ahmadessawii06 <ahmadessawii06@gmail.com>
2026-03-22 16:20:37 +0200

    upload ai-question-in-ai-page

--- src/pages/Ai.tsx
+++ src/pages/Ai.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Ai_Header } from '../components/AI/Ai_Header';
-
-
+import React from "react";
+import { Ai_Header } from "../components/AI/Ai_Header";
+import {Ai_Question}  from "../components/AI/Ai_Question";
 export default function Ai() {
-    return (
-    
-            <Ai_Header />
-
-    );
+  return (
+    <>
+      <Ai_Header />
+      <Ai_Question />
+    </>
+  );
 }
