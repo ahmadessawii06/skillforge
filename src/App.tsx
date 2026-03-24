6b4888755e21141218b26c40953c2119fa45405a
ahmadessawii06 <ahmadessawii06@gmail.com>
2026-03-24 19:05:26 +0200

    fix app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+
+import Header from './components/common/Header'
+import { Routes, Route } from 'react-router-dom'
+import Ai from './pages/Ai'
+import Home from './pages/Home'
+import Anlysis from './pages/Anlysis'
+import CV from './pages/CV'
+import Login from './pages/Login'
+import Plans from './pages/Plans'
+import PinkTeam from './pages/PinkTeam'
+
+
 
 function App() {
-  
+
   return (
     <>
-     
+      <Header />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/ai" element={<Ai />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/anlysis" element={<Anlysis />} />
+        <Route path="/cv" element={<CV />} />
+        <Route path="/plans" element={<Plans />} />
+        <Route path="/pinkteam" element={<PinkTeam />} />
+      </Routes>
+
     </>
   )
 }
