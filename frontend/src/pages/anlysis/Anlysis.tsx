a31f7ce20aa0f899f53547fe42d1befb907def88
ahmadessawii06 <ahmadessawii06@gmail.com>
2026-03-27 15:59:27 +0200

    remove analys header

--- frontend/src/pages/anlysis/Anlysis.tsx
+++ frontend/src/pages/anlysis/Anlysis.tsx
@@ -1,5 +1,5 @@
 import '../../pages/anlysis/Anlysis.css';
-import AnalysisHeader from '../../components/anlysis/AnalysisHeader';
+// import AnalysisHeader from '../../components/anlysis/AnalysisHeader';
 import AnalysisFooter from '../../components/anlysis/AnalysisFooter';
 import PerformanceTrend from '../../components/anlysis/PerformanceTrend';
 import SkillCards from '../../components/anlysis/SkillCards';
@@ -8,7 +8,7 @@ import KeywordAlignment from '../../components/anlysis/KeywordAlignment';
 export default function Anlysis() {
   return (
     <div className="container py-4">
-      <AnalysisHeader />
+      {/* <AnalysisHeader /> */}
       <SkillCards />
       <div className="row g-4 mb-5">
         <PerformanceTrend />
