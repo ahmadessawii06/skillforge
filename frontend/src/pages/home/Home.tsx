f42556e4215641460e3a08d72e7ed6f3fa3eaa17
islam <s12429409@stu.najah.edu>
2026-03-27 04:10:55 +0200

    Convert all components to Bootstrap

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,7 +1,49 @@
-export default function Home() {
-    return (
-        <div>
-            <h1>Home Page</h1>
+import Sidebar from '../../components/home/Sidebar';
+import Header from '../../components/home/Header';
+import HeroSection from '../../components/home/HeroSection';
+import StatsGrid from '../../components/home/StatsGrid';
+import RecentInterviewsTable from '../../components/home/RecentInterviewsTable';
+import SkillProgress from '../../components/home/SkillProgress';
+import ReadyToPractice from '../../components/home/ReadyToPractice';
+
+const Home = () => {
+  return (
+    <div className="d-flex vh-100 overflow-hidden" style={{ backgroundColor: '#f8f9fa' }}>
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* Main content */}
+      <main className="flex-grow-1 d-flex flex-column overflow-auto">
+        {/* Header */}
+        <Header />
+
+        {/* Page content */}
+        <div className="p-4 p-lg-5">
+          {/* Hero Section */}
+          <HeroSection />
+
+          {/* Stats Grid */}
+          <div className="my-4">
+            <StatsGrid />
+          </div>
+
+          {/* Lower section */}
+          <div className="row g-4">
+            {/* Recent Interviews Table */}
+            <div className="col-12 col-lg-8">
+              <RecentInterviewsTable />
+            </div>
+
+            {/* Skill Progress & Ready To Practice */}
+            <div className="col-12 col-lg-4 d-flex flex-column gap-4">
+              <SkillProgress />
+              <ReadyToPractice />
+            </div>
+          </div>
         </div>
-    );
-}
+      </main>
+    </div>
+  );
+};
+
+export default Home;
