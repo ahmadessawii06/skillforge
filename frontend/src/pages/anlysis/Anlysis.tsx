bd210e5450a74315e3a03468eb79e4f0354101a8
Noor kh <s12400229@stu.najah.edu>
2026-03-27 11:39:22 +0200

    feat(analysis): fix dashboard layout and restore component metrics

---
+++ frontend/src/pages/anlysis/Anlysis.tsx
@@ -0,0 +1,20 @@
+import '../../pages/anlysis/Anlysis.css';
+import AnalysisHeader from '../../components/anlysis/AnalysisHeader';
+import AnalysisFooter from '../../components/anlysis/AnalysisFooter';
+import PerformanceTrend from '../../components/anlysis/PerformanceTrend';
+import SkillCards from '../../components/anlysis/SkillCards';
+import KeywordAlignment from '../../components/anlysis/KeywordAlignment';
+
+export default function Anlysis() {
+  return (
+    <div className="container py-4">
+      <AnalysisHeader />
+      <SkillCards />
+      <div className="row g-4 mb-5">
+        <PerformanceTrend />
+        <KeywordAlignment />
+      </div>
+      <AnalysisFooter />
+    </div>
+  );
+}
