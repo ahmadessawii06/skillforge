7fc4c0074f250fdabf161989b8d7b43912ab227d
Noor kh <s12400229@stu.najah.edu>
2026-04-24 20:57:42 +0300

    refactor: simplify layout by removing conditional rendering of Header and Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
-import Header from "./components/common/header/Header";
-import { Footer } from "./components/common/footer/Footer";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Ai from "./pages/ai/Ai";
 import Home from "./pages/home/Home";
 import Anlysis from "./pages/anlysis/Anlysis";
@@ -8,16 +6,12 @@ import CV from "./pages/cv/CV";
 import Login from "./pages/login/Login";
 import Plans from "./pages/plans/Plans";
 import PinkTeam from "./pages/pinkteam/PinkTeam";
+import Header from "./components/common/header/Header";
 
 function App() {
-  const location = useLocation();
-
-  const hideLayout = location.pathname === "/";
-
   return (
     <>
-      {!hideLayout && <Header />}
-
+      <Header />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/ai" element={<Ai />} />
@@ -27,8 +21,6 @@ function App() {
         <Route path="/plans" element={<Plans />} />
         <Route path="/pinkteam" element={<PinkTeam />} />
       </Routes>
-
-      {!hideLayout && <Footer />}
     </>
   );
 }
