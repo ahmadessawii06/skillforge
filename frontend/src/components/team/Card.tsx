bff906d54e269b4bde94b80d1107bcf774950939
ahmadessawii06 <ahmadessawii06@gmail.com>
2026-05-05 18:35:48 +0300

    update(frontend:team): Add team page with member cards and styling

--- frontend/src/components/team/Card.tsx
+++ frontend/src/components/team/Card.tsx
@@ -12,15 +12,15 @@ export default function Card({ name, role, img, github }: CardProps) {
     <div className="card team-card shadow-sm h-100">
       <img src={img} className="card-img-top team-img" alt={name} />
       <div className="card-body text-center">
-        <h5 className="card-title mb-2">{name}</h5>
+        <h5 className="card-title mb-2 mt-3">{name}</h5>
         <p className="card-text text-muted">{role}</p>
         <a
           href={github}
-          className="btn btn-outline-dark"
+          className="btn btn-outline-dark github-btn"
           target="_blank"
           rel="noopener noreferrer"
         >
-          View GitHub
+         <i className="fab fa-github me-2 "></i><span>View GitHub</span>  
         </a>
       </div>
     </div>
