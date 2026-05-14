97b4c10ffe6f74e1676f56953137b900d88c3847
ahmadessawii06 <ahmadessawii06@gmail.com>
2026-05-15 01:22:24 +0300

    update(frontend): update icons to new design

--- frontend/src/components/common/header/Header.tsx
+++ frontend/src/components/common/header/Header.tsx
@@ -1,6 +1,7 @@
 import { NavLink, Link } from "react-router-dom";
 import "./Header.css";
 
+
 export default function Header() {
 
   const fullName = localStorage.getItem("fullName") || "User";
@@ -20,8 +21,9 @@ export default function Header() {
 
       <div className="logo">
 
-        <span className="material-symbols-outlined">
-          psychology
+        <span > 
+
+          <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="none" stroke="blue" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sparkles-icon lucide-sparkles"><path d="M11.017 2.814a1 1 0 0 1 1.966 0l1.051 5.558a2 2 0 0 0 1.594 1.594l5.558 1.051a1 1 0 0 1 0 1.966l-5.558 1.051a2 2 0 0 0-1.594 1.594l-1.051 5.558a1 1 0 0 1-1.966 0l-1.051-5.558a2 2 0 0 0-1.594-1.594l-5.558-1.051a1 1 0 0 1 0-1.966l5.558-1.051a2 2 0 0 0 1.594-1.594z" /><path d="M20 2v4" /><path d="M22 4h-4" /><circle cx="4" cy="20" r="2" /></svg>
         </span>
 
         <Link
@@ -106,13 +108,13 @@ export default function Header() {
 
         {/* PROFILE AVATAR */}
 
-     
+
         {/* LOGOUT */}
 
-       
 
 
-     <Link
+
+        <Link
           to="/cv"
           className="logout"
           onClick={scrollToTop}
@@ -121,14 +123,14 @@ export default function Header() {
         </Link>
 
 
-         <Link
+        <Link
           to="/"
           className="logout"
           onClick={scrollToTop}
         >
           Log out
         </Link>
-           <Link
+        <Link
           to="/profile"
           className="profile-avatar"
           onClick={scrollToTop}
